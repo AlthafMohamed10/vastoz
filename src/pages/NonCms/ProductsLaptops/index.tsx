17e5827bbb727b3dcc01216a94b687d89b8ee947
mohamed althaf hussain <althaf@valorpaytech.com>
2023-09-26 17:54:19 +0530

    Fixed bugs

--- src/pages/NonCms/ProductsLaptops/index.tsx
+++ src/pages/NonCms/ProductsLaptops/index.tsx
@@ -6,27 +6,27 @@ import FiltersLaptops from './Filters';
 const ProductsLaptops = () => {
   const [item, setItem] = useState(Products);
 
-  const menuItems = [...new Set(Products.map((Val) => Val.category))];
+  const menuItems = [new Set(Products.map((Val) => Val.category))];
   console.log(menuItems);
-  
-  const filterItem = (curcat:any) => {
+
+  const filterItem = (curcat: any) => {
     const newItem = Products.filter((newVal) => {
       return newVal.category === curcat;
     });
     setItem(newItem);
-    
+
   };
   return (
     <div>
       <div className="container-fluid mt-5 mb-5">
-           <div className="row g-2">
-            <FiltersLaptops 
-              filterItem={filterItem}
-              // setItem={setItem}
-              // menuItems={menuItems}
-              />
-              <ProductItems item={item} />
-           </div>
+        <div className="row g-2">
+          <FiltersLaptops
+            filterItem={filterItem}
+          // setItem={setItem}
+          // menuItems={menuItems}
+          />
+          <ProductItems item={item} />
+        </div>
       </div>
     </div>
   )
